3dc7c40edfeb9b72b63eec85cf4be75058e5321f
Christian Simonsen <crsdev@gmail.com>
2023-01-31 14:17:39 +0100

    notater i siste liten

--- Dictionary.mjs
+++ Dictionary.mjs
@@ -3,6 +3,23 @@ class Dictionary{
 
     constructor(...languages){
         this.languagefiles = languages
+        this.dictionary = null;
+    }
+
+    setLanguage = function(){
+        // Velger språket som skal brukes.
+
+        // Laster språket fra riktig fil (se this.languages)
+    }
+
+    get = function(key){
+        // Henter riktig verdi basert på key i dictionary.
+        return this.dictionary[key];
+    }
+
+    keys = function(){
+        // retunerer alle keys i dictionary 
+        return Object.keys( this.dictionary);
     }
 
     
